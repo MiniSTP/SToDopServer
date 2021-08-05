56f1cf4d292812fd52552f8c3c649f8ae0eae7a0
Quang0202 <quang.dovan0202@gmail.com>
2021-08-05 17:31:48 +0700

    fix mongo

--- src/main/java/com/stodop/demo/DemoApplication.java
+++ src/main/java/com/stodop/demo/DemoApplication.java
@@ -6,11 +6,15 @@ import com.stodop.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class})
 @RestController
 public class DemoApplication implements CommandLineRunner {
 
