efb40ffad9b623b7970de3eefe891e59a9a97f35
cuongloveyou <xoahetht1998@gmail.com>
2021-08-20 17:43:23 +0700

    undo validateToken method

--- src/main/java/com/stodop/demo/component/JwtTokenProvider.java
+++ src/main/java/com/stodop/demo/component/JwtTokenProvider.java
@@ -1,18 +1,13 @@
 package com.stodop.demo.component;
 
-import com.stodop.demo.dao.UserDao;
-import com.stodop.demo.model.Users;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 @Component
 public class JwtTokenProvider {
@@ -20,8 +15,7 @@ public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String secret;
     public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
-    @Autowired
-    private UserDao userDao;
+
     public String genarateJwtToken(String email) {
         Map<String, Object> claims = new HashMap<>();
         return Jwts.builder().setClaims(claims).setSubject(email).setIssuedAt(new Date(System.currentTimeMillis()))
@@ -33,16 +27,7 @@ public class JwtTokenProvider {
     }
     public boolean validateToken(String authToken) {
         try {
-            String jwtToken = null;
-            if (StringUtils.hasText(authToken) && authToken.startsWith("Bearer ")) {
-                jwtToken = authToken.substring(7);
-            }
-            String email= getEmail(jwtToken);
-            List<Users> users= userDao.findByEmail(email);
-            if(users==null|| users.isEmpty()){
-                throw new MalformedJwtException("Email Not Found");
-            }
-            Jwts.parser().setSigningKey(secret).parseClaimsJws(jwtToken);
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException e) {
             logger.error("Invalid JWT signature: {}", e.getMessage());
